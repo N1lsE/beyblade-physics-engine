273d5ff2c0777ea45fdab81125206808d9ae0a35
nils <nilseckerle2001@gmail.com>
2024-02-23 22:03:26 +0100

    something is working

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use std::{
 };
 
 mod rectangle_2d;
-use rectangle_2d::{Drawable, Rectangle2D};
+use rectangle_2d::{DOType, DrawableObject, Rectangle2D};
 
 mod winsdl;
 
 fn main() {
     const TARGET_FPS: u32 = 60;
     const FRAME_TIME: Duration = Duration::from_secs(1 / TARGET_FPS as u64);
-    const SCREEN_WIDTH: usize = 800;
-    const SCREEN_HEIGHT: usize = 600;
+    const SCREEN_WIDTH: usize = 1000;
+    const SCREEN_HEIGHT: usize = 800;
 
     let mut winsdl = Winsdl::new(SCREEN_WIDTH, SCREEN_HEIGHT).unwrap();
 
@@ -31,25 +31,9 @@ fn main() {
     canvas.set_draw_color(Color::RGB(255, 255, 255));
     canvas.clear();
 
-    let mut drawables: Vec<Box<dyn Drawable>> = Vec::new();
+    let mut drawables: Vec<Box<dyn DrawableObject>> = Vec::new();
 
-    let rec1 = Box::new(Rectangle2D {
-        pos_x: 300,
-        pos_y: 400,
-        width: 100,
-        height: 200,
-        col: Color::RGB(0, 255, 0),
-    });
-    drawables.push(rec1);
-
-    let rec2 = Box::new(rectangle_2d::Rectangle2D {
-        pos_x: 100,
-        pos_y: 400,
-        width: 100,
-        height: 200,
-        col: Color::RGB(0, 255, 0),
-    });
-    drawables.push(rec2);
+    init(&mut drawables);
 
     'running: loop {
         for event in winsdl.event_pump.poll_iter() {
@@ -59,14 +43,45 @@ fn main() {
             }
         }
 
-        update(&mut canvas, &mut background, &drawables, FRAME_TIME);
+        update(&mut canvas, &mut background, &mut drawables, FRAME_TIME);
     }
+}
+fn init(drawables: &mut Vec<Box<dyn DrawableObject>>) {
+    let rec1 = Box::new(Rectangle2D::new(
+        1000,
+        400,
+        100,
+        200,
+        Color::RGB(0, 255, 0),
+        DOType::Pipe,
+    ));
+    drawables.push(rec1);
+
+    let rec2 = Box::new(Rectangle2D::new(
+        2000,
+        400,
+        100,
+        200,
+        Color::RGB(0, 255, 0),
+        DOType::Pipe,
+    ));
+    drawables.push(rec2);
+
+    let rec3 = Box::new(Rectangle2D::new(
+        3000,
+        400,
+        100,
+        200,
+        Color::RGB(0, 255, 0),
+        DOType::Pipe,
+    ));
+    drawables.push(rec3);
 }
 
 fn update(
     canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
     background: &mut sdl2::render::Texture<'_>,
-    drawables: &Vec<Box<dyn Drawable>>,
+    drawables: &mut Vec<Box<dyn DrawableObject>>,
     frame_time: Duration,
 ) {
     let frame_start = Instant::now();
@@ -74,6 +89,23 @@ fn update(
     // Clear Canvas
     canvas.set_draw_color(Color::RGB(255, 255, 255));
     canvas.clear();
+
+    // Update Pipes
+    for drawable in drawables.iter_mut() {
+        match drawable.get_object_type() {
+            DOType::Pipe => {
+                if drawable.get_pos_x() <= 0 - drawable.get_width() as i32 {
+                    drawable.set_pos_x(3000);
+                } else {
+                    drawable.set_pos_x(drawable.get_pos_x() - 1);
+                }
+            }
+            DOType::Player => {}
+            _ => {
+                panic!("Not Implemented DOType update")
+            }
+        }
+    }
 
     // Draw on Canvas
     canvas
@@ -97,7 +129,7 @@ fn update(
 
 fn draw(
     canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-    drawables: &Vec<Box<dyn Drawable>>,
+    drawables: &Vec<Box<dyn DrawableObject>>,
 ) {
     // Draw Background
     canvas.set_draw_color(Color::RGB(255, 255, 255));
--- src/rectangle_2d.rs
+++ src/rectangle_2d.rs
@@ -1,22 +1,100 @@
+use gl::TEXTURE_HEIGHT;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 
 pub struct Rectangle2D {
-    pub pos_x: i32,
-    pub pos_y: i32,
-    pub width: u32,
-    pub height: u32,
-    pub col: Color,
+    pos_x: i32,
+    pos_y: i32,
+    width: u32,
+    height: u32,
+    col: Color,
+    object_type: DOType,
 }
 
-pub trait Drawable {
+pub enum DOType {
+    Pipe,
+    Player,
+}
+
+impl Rectangle2D {
+    pub fn new(
+        pos_x: i32,
+        pos_y: i32,
+        width: u32,
+        height: u32,
+        col: Color,
+        object_type: DOType,
+    ) -> Rectangle2D {
+        Rectangle2D {
+            pos_x: pos_x,
+            pos_y: pos_y,
+            width: width,
+            height: height,
+            col: col,
+            object_type: object_type,
+        }
+    }
+}
+
+pub trait DrawableObject {
     fn draw(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>);
+    fn get_pos_x(&self) -> i32;
+    fn get_pos_y(&self) -> i32;
+    fn get_width(&self) -> u32;
+    fn get_height(&self) -> u32;
+    fn get_col(&self) -> Color;
+    fn get_object_type(&self) -> &DOType;
+
+    fn set_pos_x(&mut self, pos_x: i32);
+    fn set_pos_y(&mut self, pos_y: i32);
+    fn set_width(&mut self, width: u32);
+    fn set_height(&mut self, height: u32);
+    fn set_col(&mut self, col: Color);
+    fn set_object_type(&mut self, object_type: DOType);
 }
 
-impl Drawable for Rectangle2D {
+impl DrawableObject for Rectangle2D {
     fn draw(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
         canvas.set_draw_color(self.col);
         let rect = Rect::new(self.pos_x, self.pos_y, self.width, self.height); // X, Y, Width, Height
         canvas.fill_rect(rect).expect("Failed to draw rectangle");
+    }
+
+    fn get_pos_x(&self) -> i32 {
+        self.pos_x
+    }
+    fn get_pos_y(&self) -> i32 {
+        self.pos_y
+    }
+    fn get_width(&self) -> u32 {
+        self.width
+    }
+    fn get_height(&self) -> u32 {
+        self.height
+    }
+    fn get_col(&self) -> Color {
+        self.col
+    }
+    fn get_object_type(&self) -> &DOType {
+        &self.object_type
+    }
+
+    fn set_pos_x(&mut self, pos_x: i32) {
+        self.pos_x = pos_x;
+    }
+    fn set_pos_y(&mut self, pos_y: i32) {
+        self.pos_y = pos_y;
+    }
+    fn set_width(&mut self, width: u32) {
+        self.width = width;
+    }
+    fn set_height(&mut self, height: u32) {
+        self.height = height;
+    }
+    fn set_col(&mut self, col: Color) {
+        self.col = col;
+    }
+    fn set_object_type(&mut self, object_type: DOType) {
+        self.object_type = object_type
     }
 }
